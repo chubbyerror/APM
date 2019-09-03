433dd0dd2beb15e5d6b6498e72f65201abbbd6ea
Maxim Shinkarev <maxim.shinkarev@cortlex.com>
2019-09-03 15:26:25 +0300

    Added new dashboard versions. Fixed getting of environment variable.

--- src/Cortlex.APM.Health.Publishers.InfluxDb/InfluxHealthCheckBuilderExtensions.cs
+++ src/Cortlex.APM.Health.Publishers.InfluxDb/InfluxHealthCheckBuilderExtensions.cs
@@ -6,12 +6,25 @@ namespace Cortlex.APM.Health.Publishers.InfluxDb
 {
     public static class InfluxHealthCheckBuilderExtensions
     {
-        public static IHealthChecksBuilder AddInfluxPublisher(this IHealthChecksBuilder builder, string endpoint,
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="applicationName">Value of IHostingEnvironment.ApplicationName</param>
+        /// <param name="environmentName">Value of IHostingEnvironment.EnvironmentName</param>
+        /// <param name="endpoint"></param>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <param name="dbVersion"></param>
+        /// <param name="dbName"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static IHealthChecksBuilder AddInfluxPublisher(this IHealthChecksBuilder builder, string applicationName, string environmentName, string endpoint,
             string login, string password, InfluxDbVersion dbVersion = InfluxDbVersion.Latest,
             string dbName = "_internal")
         {
             builder.Services.AddSingleton(sp =>
-                (IHealthCheckPublisher) new InfluxDbPublisher(endpoint, login, password, dbVersion, dbName));
+                (IHealthCheckPublisher) new InfluxDbPublisher(endpoint, login, password, dbVersion, dbName, applicationName, environmentName));
             return builder;
         }
     }
